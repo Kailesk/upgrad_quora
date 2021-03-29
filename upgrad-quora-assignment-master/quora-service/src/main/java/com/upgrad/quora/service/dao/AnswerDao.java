161bfdbebd29bdfe18a5578f8fdfb73cb4a8f65a
Kailesk <kaileskkhumarvs@gmail.com>
2021-03-29 23:31:04 +0530

    making changes for contoller

---
+++ upgrad-quora-assignment-master/quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,50 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.AnswerEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AnswerDao {
+
+  @PersistenceContext private EntityManager entityManager;
+
+  public AnswerEntity createAnswer(AnswerEntity answerEntity) {
+    entityManager.persist(answerEntity);
+    return answerEntity;
+  }
+
+  public AnswerEntity getAnswerById(final String answerId) {
+    try {
+      return entityManager
+              .createNamedQuery("answerByUuid", AnswerEntity.class)
+              .setParameter("uuId", answerId)
+              .getSingleResult();
+    } catch (Exception nre) {
+      return null;
+    }
+  }
+
+  public void updateAnswer(AnswerEntity answerEntity) {
+    entityManager.merge(answerEntity);
+  }
+
+  public AnswerEntity deleteAnswer(final String answerId) {
+    AnswerEntity deleteAnswer = getAnswerById(answerId);
+    if (deleteAnswer != null) {
+      entityManager.remove(deleteAnswer);
+    }
+    return deleteAnswer;
+  }
+
+
+  public List<AnswerEntity> getAllAnswersToQuestion(final String questionId) {
+    return entityManager
+            .createNamedQuery("getAllAnswersToQuestion", AnswerEntity.class)
+            .setParameter("questionUuid", questionId)
+            .getResultList();
+  }
+}
---
+++ upgrad-quora-assignment-master/quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -0,0 +1,93 @@
+package com.upgrad.quora.service.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "QUESTION", schema = "public")
+@NamedQueries({
+  @NamedQuery(name = "allQuestions", query = "select q from QuestionEntity q"),
+  @NamedQuery(
+      name = "findByQuestionUuId",
+      query = "select q from QuestionEntity q where q.uuid = :questionUuid"),
+  @NamedQuery(
+      name = "allQuestionsByUserId",
+      query = "select u from QuestionEntity u where u.user.uuid= :userId")
+})
+public class QuestionEntity implements Serializable {
+
+  @Id
+  @Column(name = "ID")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "UUID")
+  @Size(max = 200)
+  @NotNull
+  private String uuid;
+
+  @Column(name = "CONTENT")
+  @NotNull
+  @Size(max = 500)
+  private String content;
+
+  @Column(name = "DATE")
+  @NotNull
+  private Timestamp date;
+
+  @ManyToOne
+  @JoinColumn(name = "USER_ID")
+  private UserEntity user;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Timestamp getDate() {
+    return date;
+  }
+
+  public void setDate(Timestamp date) {
+    this.date = date;
+  }
+
+  public UserEntity getUser() {
+    return user;
+  }
+
+  public void setUser(UserEntity user) {
+    this.user = user;
+  }
+}
